f60f219b3b27ec399f246cff0b2ecba9486c0c78
Ali-Elleithy1 <ali.kh1615@gmail.com>
2025-08-17 15:59:40 +0300

    Add new expenses and display expenses

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,11 +12,29 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Cinema',
+      amount: 400,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Summer Vacation',
+      amount: 10000,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
+
   @override
   Widget build(context) {
     return Scaffold(
       body: Column(
-        children: [Text('Expenses Chart'), Text('Expenses list...')],
+        children: [
+          Text('Expenses Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
       ),
     );
   }
