8ee3969e35e7f1dc5c947622649e584ff9723cd8
Ali-Elleithy1 <ali.kh1615@gmail.com>
2025-09-13 16:55:21 +0300

    Add themes to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 65, 90, 119),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 119, 141, 169),
+  brightness: Brightness.dark,
+);
+
 void main() {
-  runApp(MaterialApp(home: Expenses()));
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+          ),
+        ),
+      ),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: TextTheme().copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black,
+            fontSize: 18,
+          ),
+          bodyMedium: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.black,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      home: Expenses(),
+    ),
+  );
 }
